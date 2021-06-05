3c7fb329c04afc6778cd17eb2420452b2757b8a5
fujisawaayano <fujisawaayano@192.168.3.27>
2021-06-05 10:12:23 +0900

    CreateNewOshiにアプリケーションスコープを作成する(1)

--- src/main/java/main/CreateNewOshi.java
+++ src/main/java/main/CreateNewOshi.java
@@ -1,14 +1,18 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.Oshi;
+import model.User;
 
 /**
  * Servlet implementation class CreateNewOshi
@@ -42,6 +46,8 @@ public class CreateNewOshi extends HttpServlet {
 		String name = request.getParameter("name");
 		String firstMoney =  request.getParameter("firstMoney");
 		String userName=request.getParameter("userName");
+		//
+		String pass=request.getParameter("pass");
 		
 		//入力された値をプロパティに設定
 		Oshi oshi = new Oshi(name, Integer.parseInt(firstMoney),userName);
@@ -52,6 +58,46 @@ public class CreateNewOshi extends HttpServlet {
 		//フォワード
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/CreateNewOshiResult.jsp");
 		dispatcher.forward(request, response);
+		
+		//アプリケーションスコープの作成,取得
+		ServletContext application = this.getServletContext();
+		
+		//入力された値をプロパティに設定
+		User user = new User(name, pass);
+		
+		List<Oshi> oshiList = null;
+		
+		//初回ログインの場合
+		if(application.getAttribute("oshi")==null) {
+			oshiList = (List<Oshi>) application.getAttribute("oshiList");
+			oshiList.add(oshi);
+			//アプリケーションコープに保存
+			application.setAttribute("oshiList", oshiList);
+			//推しをoshiMenに追加する
+			
+			//貢いだ総額
+			int totalMoney=Integer.valueOf(firstMoney);
+			user.addTotalMoney(totalMoney);
+		}else {
+			//２回目以降のログイン
+			oshiList.add(oshi);
+			//アプリケーションコープに保存
+			application.setAttribute("oshiList", oshiList);
+			//２回目以降のログイン
+			Oshi nextOshi = (Oshi)application.getAttribute("oshi");
+			//推しをoshiMenに追加する
+			
+			//貢いだ総額
+			int totalMoney= nextOshi.getTotalMoney() + Integer.valueOf(firstMoney);
+			user.addTotalMoney(totalMoney);
+		}
+		
+		//アプリケーションコープに保存
+		application.setAttribute("oshi", oshi);
+		
+		//結果画面に表示する
+		
+		
 		
 	}
 
